bb6348aab62764d1818b9fdc5cd268c383fbec0b
juniorlimajj <josealbertorodriguesjunior@gmail.com>
2023-02-13 17:36:35 +0000

    Initial commit - Structure

---
+++ src/main/java/io/github/juniorlimajj/Application.java
@@ -0,0 +1,10 @@
+package io.github.juniorlimajj;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
